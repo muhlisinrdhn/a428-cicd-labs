952a432bb8debdc88cf3f369cae49464268b3b26
Ramadhan <67773748+muhlisinrdhn@users.noreply.github.com>
2023-06-10 02:04:59 +0700

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-echo 'Break 1 Minute before close app'
-sleep 1m
-
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
